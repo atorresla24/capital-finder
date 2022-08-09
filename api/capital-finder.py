fcbef246c7123fd6190d719d954226fd0639b5c9
Alec Torres <atorresla24@gmail.com>
2022-08-08 20:39:20 -0400

    lab

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -10,16 +10,16 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         query_dict = dict(query_string_list)
 
-        if "capital" in query_dict:
+        if "name" in query_dict:
             url = "https://restcountries.com/v3.1/name/"
-            query = query_dict["capital"]
+            query = query_dict["name"]
 
-            response = requests.get(url + query_dict["capital"])
+            response = requests.get(url + query_dict["name"])
             data = response.json()
 
             parsed_capital = data[0]["capital"]
             country = str(parsed_capital[0])
-            message = f"{query} is the capital {country}"
+            message = f"{query} is the capital of {country}"
 
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
--- api/country-finder.py
+++ api/country-finder.py
@@ -10,16 +10,16 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         query_dict = dict(query_string_list)
 
-        if "name" in query_dict:
+        if "capital" in query_dict:
             url = "https://restcountries.com/v3.1/capital/"
-            query = query_dict["name"]
+            query = query_dict["capital"]
 
-            response = requests.get(url + query_dict["name"])
+            response = requests.get(url + query)
             data = response.json()
 
             country_name = data[0]["name"]
-            country = str(country_name("common"))
-            message = f"{query} is the capital {country}"
+            country = str(country_name["common"])
+            message = f"The capital of {country} is {query}"
 
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
